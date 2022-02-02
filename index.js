c7ecfe78294042bddaa8d1f26964b888d447b5fb
Alisa Gezalova <alisa.gezalova@gmail.com>
2022-02-01 22:20:49 -0700

    commit tree cf4230e4ecc5f82f68379dd62a878f7568300868

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random() * 10);
+
+const num3 = 14;
+const num4 = 5;
+
+var mod = num3 % num4;
+
+var max = Math.max(1, 2, 3, 4, 5, 20);
